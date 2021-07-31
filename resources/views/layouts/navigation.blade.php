17e290e506bc6e1d95e35b161793a22f476169fe
amangeldiyev <amangeldiyev.amantur@gmail.com>
2021-08-01 04:04:35 +0500

    responsive nav

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -90,6 +90,15 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('total-funding-rate')" :active="request()->routeIs('total-funding-rate')">
+                Total funding rate
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('open')" :active="request()->routeIs('open')">
+                Open position
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('close')" :active="request()->routeIs('close')">
+                Close position
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
